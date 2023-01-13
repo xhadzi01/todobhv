f2f4bdf3081f26e6a9074dba65cc04a3faa7167e
Hadzima, Jaroslav <jaroslav.hadzima@thermofisher.com>
2023-01-13 21:16:47 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module todoapi
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"todoapi/state"
+)
+
+func main() {
+	fmt.Println("Hiiii")
+	var aa state.Task
+
+	aa.ID = 15
+
+	fmt.Println(aa)
+}
